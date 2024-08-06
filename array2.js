565ad6e202f67f7c782e5d0d648fec1b3d4dc19a
abdulla alhashmi <abdulla@alhashmi.qa>
2024-08-06 18:09:51 +0300

    Task2

---
+++ array2.js
@@ -0,0 +1,18 @@
+/**
+ * isArrayLengthOdd(numbers):
+ * - receives array `numbers`
+ * - returns true if array has an odd length
+  let num = isOdd(12);
+console.log(num);number of elements
+ * - returns false otherwise
+ *
+ * e.g.
+ * isArrayLengthOdd([1, 2, 3]) -> true
+ * isArrayLengthOdd([1, 2, 3, 4]) -> flase
+ */
+function isArrayLengthOdd(numbers) {
+  // const length = numbers.length;
+
+  // Check if the length is odd
+  return length % 2 !== 0;
+}
